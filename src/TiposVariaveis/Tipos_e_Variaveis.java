c9ab5e491ca0924cc32b204df10d6688c899167e
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-08 20:02:37 -0300

    Ternario

--- src/TiposVariaveis/Tipos_e_Variaveis.java
+++ src/TiposVariaveis/Tipos_e_Variaveis.java
@@ -3,7 +3,15 @@ package TiposVariaveis;
 public class Tipos_e_Variaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a,b;
+		String result ;
+		
+		a = 5;
+		b= 7;
+		
+		result =  a != b? a+" e igual "+b : a+" Nao e igual "+b;  
+		System.out.println(result);
 
 	}
 
