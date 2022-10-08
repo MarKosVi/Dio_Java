5398f7fe1f5ad325c220664483a9c6aba4bb6461
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-08 20:46:37 -0300

    Metodos

--- src/TiposVariaveis/Tipos_e_Variaveis.java
+++ src/TiposVariaveis/Tipos_e_Variaveis.java
@@ -4,15 +4,16 @@ public class Tipos_e_Variaveis {
 
 	public static void main(String[] args) {
 		
-		String a,b;
-		boolean result ;
-		
-		a = "Ola";
-		b = "Ola";
-		
-		result = (a.equals(b));  
-		System.out.println(result);
+		imprimir(String.valueOf(somar(10, 50)));
 
+	}
+	
+	public static double somar (int num1, int num2) {
+		return num1+num2;
+	}
+	
+	public static void imprimir(String text) {
+		System.out.println(text);
 	}
 
 }
