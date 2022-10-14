13b604bbbb3aabe39668b9bd1c2041001aa54787
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-14 00:49:49 -0300

    atividade 02

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -6,23 +6,15 @@ public class main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		imprimir(scan);
 		
-	}
-	
-	public static void imprimir(Scanner scan) {
+		int nota;
 		
-		String nome;
-		int idade;
-		
-		while(true) {
-			System.out.println("Informe o Nome");
-			nome = scan.next();
-			if(nome.equals("0"))break;
-			System.out.println("Informe a idade");
-			idade = scan.nextInt();
-			System.out.println("Nome: "+nome+" idade: "+idade+"");
+		System.out.println("Informe o valor da nota ");
+		nota = scan.nextInt();
+		while(nota < 0 || nota > 10) {
+			System.out.println("Valor "+nota+" Invalido, informe uma nota valida");
+			nota = scan.nextInt();
 		}
-		System.out.println("Programa finalizado");
+		System.out.println("Nota: "+nota);
 	}
 }
