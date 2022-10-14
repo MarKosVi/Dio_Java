3c0598f11d2b163262da512d40528dea2b7eeef7
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-14 01:26:03 -0300

    atividade 04

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -8,26 +8,24 @@ public class main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int[] arr = new int[5];
-		int temp = 0;
-		int media = 0;
+		int num, qtdNum, par = 0, impar = 0, soma = 0;
 		
-		for(int i = 0; i<arr.length; i++) {
-			System.out.println("Informe o "+i+" valor");
-			arr[i] = scan.nextInt();
-			media += arr[i];
-			
-			if(i == 0) {
-				temp = arr[i];
-			}
-			
-			if(temp < arr[i]) {
-				temp = arr[i];
+		System.out.println("Informe a quantidade de numeros a serem informados ");
+		qtdNum = scan.nextInt();
+		
+		for(int i = 0; i < qtdNum; i++) {
+			System.out.println("Informe o "+(i+1)+" Numero");
+			num = scan.nextInt();
+			soma += num;
+			if(num % 2 == 0) {
+				par++;
+			}else {
+				impar++;
 			}
 		}
-		media /=arr.length;
 		
-		System.out.println(" maior nota e: "+temp+" e a media e: "+media);
+		System.out.println("Foram identificados "+par+" numeros par e "+impar+" impar");
+	
 	}		
 		
 }
