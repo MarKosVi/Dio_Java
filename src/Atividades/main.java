e06483429e816ab73e070ecec82ce2f139637f5e
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-14 01:49:07 -0300

    atividade 05

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -8,15 +8,15 @@ public class main {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int num = 0;
+		int num, mult = 1;
 		
-		System.out.println("Informe o numero");
+		System.out.println("Calculo de fatorial \n");
 		num = scan.nextInt();
-		
-		for(int i = 1; i<=10; i++) {
-			System.out.println(num+" X "+ i+" = "+(num*i) );
+		for(int i = num; i> 1; i--) {
+			mult *= i;
+			System.out.println(i+" x "+(i-1)+ " = "+mult );
 		}
+		System.out.println("O valor do fatorial  de "+num+"! e "+mult );
 	
 	}		
-		
 }
