13d8f58f27e1a43b1d40444e357d03de2a246db9
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-14 00:16:37 -0300

    atividade 01-2

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -5,30 +5,24 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-
 		Scanner scan = new Scanner(System.in);
-		
-		String nome;
-		int idade;
-		boolean temp = true;
-		
-		while(temp) {
-			System.out.println("Informe o Nome");
-			nome = scan.next();
-			if(nome.equals("0")) {
-				System.out.println("Programa finalizado");
-				temp = false;
-				break;
-			}
-			System.out.println("Informe a idade");
-			idade = scan.nextInt();
-			imprimir(nome, idade);	
-		}
-		
+		imprimir(scan);
 		
 	}
 	
-	public static void imprimir(String nome, int idade) {
-		System.out.println("Nome: "+nome+" idade: "+idade+"");
+	public static void imprimir(Scanner scan) {
+		
+		String nome;
+		int idade;
+		
+		while(true) {
+			System.out.println("Informe o Nome");
+			nome = scan.next();
+			if(nome.equals("0"))break;
+			System.out.println("Informe a idade");
+			idade = scan.nextInt();
+			System.out.println("Nome: "+nome+" idade: "+idade+"");
+		}
+		System.out.println("Programa finalizado");
 	}
 }
