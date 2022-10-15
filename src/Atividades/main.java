3a4ebcb00aaef21a0eba288d8130964741a12371
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-15 16:50:54 -0300

    atividades 04

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -1,5 +1,6 @@
 package Atividades;
 
+
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,20 +8,32 @@ public class main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		Random random = new Random();    
+		Random random = new Random();
+
+		int[][] matriz = new int[4][4];
 		
-		int[] num = new int[20];
-		
-		for(int i = 0; i<20; i++) {
-			num[i] = random.nextInt(100);
+		for(int i = 0; i<4; i++ ) {
+			for(int j = 0; j<4; j++ ) {
+				matriz[i][j] = random.nextInt(9);
+			}
 		}
 		
-		for(int i = 0; i<20; i++) {
-			System.out.print(num[i]+" ");
+		
+		for(int i = 0; i<4; i++ ) {
+			System.out.print(matriz[0][i]);
 		}
-		System.out.println("\n Seus sucessores sao: ");
-		for(int i = 0; i<20; i++) {
-			System.out.print((++num[i])+" ");
-		}			
+		System.out.print("\n");
+		for(int i = 0; i<4; i++ ) {
+			System.out.print(matriz[1][i]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i<4; i++ ) {
+			System.out.print(matriz[2][i]);
+		}
+		System.out.print("\n");
+		for(int i = 0; i<4; i++ ) {
+			System.out.print(matriz[3][i]);
+		}
+		
 	}
 }
