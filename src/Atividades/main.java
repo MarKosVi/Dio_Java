67ff8316a9c9e7de52a5f510a9f5c922ff96c4d8
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-15 18:32:30 -0300

    metodos atividade 01

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -10,30 +10,10 @@ public class main {
 		Scanner scan = new Scanner(System.in);
 		Random random = new Random();
 
-		int[][] matriz = new int[4][4];
-		
-		for(int i = 0; i<4; i++ ) {
-			for(int j = 0; j<4; j++ ) {
-				matriz[i][j] = random.nextInt(9);
-			}
-		}
-		
-		
-		for(int i = 0; i<4; i++ ) {
-			System.out.print(matriz[0][i]+" ");
-		}
-		System.out.print("\n");
-		for(int i = 0; i<4; i++ ) {
-			System.out.print(matriz[1][i]+" ");
-		}
-		System.out.print("\n");
-		for(int i = 0; i<4; i++ ) {
-			System.out.print(matriz[2][i]+" ");
-		}
-		System.out.print("\n");
-		for(int i = 0; i<4; i++ ) {
-			System.out.print(matriz[3][i]+" ");
-		}
-		
+		int i, num = 5;
+        for (i = 0; i < 3; i++) {
+            num += i;
+            System.out.print(i);
+        }
 	}
 }
