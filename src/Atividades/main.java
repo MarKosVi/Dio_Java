f58c85061f49b488e67f76da748072361d316357
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-15 16:33:31 -0300

    atividade 03

--- src/Atividades/main.java
+++ src/Atividades/main.java
@@ -1,35 +1,26 @@
 package Atividades;
 
-import java.lang.reflect.Array;
+import java.util.Random;
 import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
+		Random random = new Random();    
 		
-		String[] palavra = new String[6];
-		String[] vogais = {"a", "e", "i", "o", "u"};
-		String[] consoantes = new String[6];
-		int qtdConsoantes = 0;
+		int[] num = new int[20];
 		
-		for (int i = 0; i<palavra.length; i++) {
-			System.out.println("Informe o "+(i+1)+" numero");
-			palavra[i] = scan.next();	
-			
-			if (!(palavra[i].equalsIgnoreCase("a") || palavra[i].equalsIgnoreCase("e") || palavra[i].equalsIgnoreCase("i") 
-					|| palavra[i].equalsIgnoreCase("o") || palavra[i].equalsIgnoreCase("u"))) {
-				consoantes[i] = palavra[i];
-				qtdConsoantes++;
-			}
-			
+		for(int i = 0; i<20; i++) {
+			num[i] = random.nextInt(100);
 		}
 		
-		for (int i = 0; i<consoantes.length; i++) {
-			if(consoantes[i] != null) {
-				System.out.print(consoantes[i]+" ");
-			}
+		for(int i = 0; i<20; i++) {
+			System.out.print(num[i]+" ");
 		}
-		System.out.print(" totalizando: "+qtdConsoantes+" consoantes" );
+		System.out.println("\n Seus sucessores sao: ");
+		for(int i = 0; i<20; i++) {
+			System.out.print((++num[i])+" ");
+		}			
 	}
 }
