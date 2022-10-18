2d0bb81345b518d4a282e7897ef52012a1f55a4c
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-18 00:16:17 -0300

    desafio 2

--- src/metodos/main.java
+++ src/metodos/main.java
@@ -6,21 +6,22 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int N;
+		Scanner number = new Scanner(System.in);
 
-        Scanner input = new Scanner(System.in);
-        N = input.nextInt();
-        
-        System.out.println(somatorio(N));
-		
-	}
-	
-	public static int somatorio(int n) {
-	      if(n == 0) {
-	    	  return 0;
-	      }else {
-	    	  return n+somatorio(n-1);
-	      }
+        int num = number.nextInt();
+        System.out.println(FizzBuzz(num));
+        // TODO: Retorne a palavra correta de acordo com o múltiplo de "num". 
+        // Caso o valor não seja múltiplo de 3 ou 5, exiba o número, conforme o enunciado.
+
+    }
+    
+    
+    static String FizzBuzz(int n) {
+      if(n%3 == 0 && n%5 == 0) return "FizzBuzz";
+      if(n%3 == 0) return "Fizz";
+      if(n%5 == 0) return "Buzz";
+      
+      return ""+n;
 	    }
 	
 }
