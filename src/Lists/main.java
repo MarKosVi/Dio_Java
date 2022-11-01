d6ff6038ceb32dd87ad48a2ac61c198cc21a6a20
Marcos Vinicio Fonseca Barros <mfonsecabarros@gmail.com>
2022-10-31 22:22:50 -0300

    Atividades List

--- src/Lists/main.java
+++ src/Lists/main.java
@@ -1,43 +1,61 @@
 package Lists;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Scanner;
+
+import java.util.*;
 
 public class main {
 
 	public static void main(String[] args) {
 		
 		Scanner in = new Scanner(System.in);
-		ArrayList<Double> notas = new ArrayList<>();
-		double soma = 0;
+		List<Integer> list = Arrays.asList(7, 8, 9, 5, 7, 0, 3);
+		ArrayList<Integer> listaNotas = new ArrayList<>();
 		
-		
-		for(int i = 1; i<= 7; i++) {
-			System.out.println("Informe a "+i+" nota");
-			notas.add(in.nextDouble());
+			System.out.println(list);	
+			
+		for(Integer l :  list) {
+			listaNotas.add(l);
 		}
+			
+		System.out.println(listaNotas);	
+		System.out.println(listaNotas.get(5));	
+		System.out.println(listaNotas.indexOf(5));	
+		listaNotas.set(4, 8);
+		System.out.println(listaNotas);	
+		listaNotas.set(listaNotas.indexOf(5), 6);
+		System.out.println(listaNotas);	
+		System.out.println(listaNotas.contains(5));
+		System.out.println(listaNotas.get(2));
+		System.out.println(Collections.min(listaNotas, null));
+		System.out.println(Collections.max(listaNotas, null));
 		
-		System.out.println(notas);
-		System.out.println(notas.indexOf(5.0));
-		notas.set(4, 8.0);
-		System.out.println(notas);
-		notas.set(5, notas.get(6));
-		System.out.println(notas);
-		System.out.println(notas.contains(5.0));
-		System.out.println(Collections.min(notas));
-		System.out.println(Collections.max(notas));
-		System.out.println(notas.get(0) + notas.get(notas.size()-1));
-		
-		for(double r : notas) {
-			soma += r;
+		Integer soma = 0;
+		for (Integer l : listaNotas) {
+			soma += l;
 		}
-		System.out.println(soma);
-		System.out.println(soma/notas.size());
-		System.out.println(notas);
-		System.out.println(notas.remove(0d));
-		System.out.println(notas);
-
-	}
-
+	
+		System.out.println(soma);	
+		
+		/*Iterator notas = listaNotas.iterator();
+		
+		while(notas.hasNext()) {
+			Integer next = (Integer) notas.next();
+			soma += next;
+		}*/
+		System.out.println(soma/listaNotas.size());	
+		listaNotas.remove(listaNotas.indexOf(0));
+		System.out.println(listaNotas);	
+		listaNotas.remove(0);
+		System.out.println(listaNotas);	
+		
+		Iterator it = listaNotas.iterator();
+		while(it.hasNext()) {
+			Integer next = (Integer) it.next();
+			if (next < 7) it.remove();
+			
+		}
+		System.out.println(listaNotas);
+		listaNotas.clear();
+		System.out.println(listaNotas);
+		System.out.println(listaNotas.isEmpty());
+		}
 }
